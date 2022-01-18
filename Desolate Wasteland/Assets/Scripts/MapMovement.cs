11a1f9729322823b5774df931137c91e8a673a26
Przemysław Majcher <s20469@pjwstk.edu.pl>
2022-01-19 00:56:22 +0100

    Basic map movement

--- Desolate Wasteland/Assets/Scripts/MapMovement.cs
+++ Desolate Wasteland/Assets/Scripts/MapMovement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class MapMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite Stationary;
+    public Sprite Moving;
+    public float speed;
+    private Vector2 destPosition;
+    private bool moving;
+    private SpriteRenderer sprite;
+
+    private void Start()
     {
-        
+        moving = false;
+        destPosition = (Vector2)transform.position;
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            destPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            destPosition = (Vector2)transform.position;
+            moving = false;
+        }
+        if (moving && (Vector2)transform.position != destPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destPosition, Time.deltaTime * speed);
+            sprite.sprite = Moving;
+        }
+        else
+        {
+            moving = false;
+            sprite.sprite = Stationary;
+        }
     }
+
+    private void OnMouseUp()
+    {
+        if (moving == false)
+        {
+            Debug.Log("Enter location");
+        }
+    }
+
 }
