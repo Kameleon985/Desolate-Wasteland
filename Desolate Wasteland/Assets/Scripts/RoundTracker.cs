a96890d94b10fa47ba390bceed42128e37930cb6
s21446 <s21446@pjwstk.edu.pl>
2022-01-18 23:26:08 +0100

    System surowców
    System surowców + podstawowe UI, rozwiązanie z przechowaniem danych do poprawy ale taki WIP jest, więc no.

---
+++ Desolate Wasteland/Assets/Scripts/RoundTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundTracker : MonoBehaviour
+{
+    public void IncrementRound()
+    {
+        TempPlayerData.CurrentRound++;
+        UIUpdate.Instance.UpdateRound(TempPlayerData.CurrentRound);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Desolate Wasteland/Assets/Scripts/TestPlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestPlayerMovement : MonoBehaviour
+{
+
+    public float playerSpeed = 2.5f;
+    public Transform shotSpawn;
+    public Camera cam;
+    
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);       
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * playerSpeed * Time.fixedDeltaTime);
+/*
+        Vector2 lookDirection = mousePos - rb.position;
+        float angleZ = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angleZ;*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Kolizja");
+        if (collision.gameObject.CompareTag("Surowiec"))
+        {
+            //Debug.Log("Kolizja z wrogiem");
+            Destroy(gameObject);
+            //Death();
+        }
+    }
+
+    private void Death()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+
+    }
+}
+
+    
+
