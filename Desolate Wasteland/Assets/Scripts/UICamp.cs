1190d6b9aac1f7a13c696e5c0e892f0d02e74bc9
s21446 <s21446@pjwstk.edu.pl>
2022-01-24 15:20:12 +0100

    CampScene
    Dodałem wersję roboczą obozu z oknem rozbudowy, najpierw trzeba się zająć sposobem przechowywania danych żeby zapisywać stan surowców, armii gracza, tury i obozu

---
+++ Desolate Wasteland/Assets/Scripts/UICamp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UICamp : MonoBehaviour
+{
+    public GameObject campView;
+    public GameObject buildingView;
+    public GameObject marketView;
+    public GameObject labView;
+    public static UICamp Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void BuildingScreenOpen()
+    {
+        buildingView.gameObject.SetActive(true);
+    }
+    public void BuildingScreenClose()
+    {
+        buildingView.gameObject.SetActive(false);
+    }
+
+    public void ReturnToMap()
+    {
+        SceneManager.LoadScene("Map&UI");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
