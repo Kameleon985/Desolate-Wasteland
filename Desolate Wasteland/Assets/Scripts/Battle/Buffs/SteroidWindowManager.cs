a2f324188e1ac73a9af5eaa39c2016f3d05accd4
s21446 <s21446@pjwstk.edu.pl>
2022-04-12 22:18:27 +0200

    Okno i toggle
    Jeszcze brak funkcjonalności

---
+++ Desolate Wasteland/Assets/Scripts/Battle/Buffs/SteroidWindowManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteroidWindowManager : MonoBehaviour
+{
+
+    public GameObject steroidManagerWindow;
+
+    public void switchWindowActive()
+    {
+        steroidManagerWindow.SetActive(!steroidManagerWindow.activeSelf);
+    }
+}
