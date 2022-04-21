66a61c460a839cc2fe0eafd672205ef532d0c4f3
Przemysław Majcher <s20469@pjwstk.edu.pl>
2022-04-21 17:59:00 +0200

    Update MeleeEnemy.cs

--- Desolate Wasteland/Assets/Scripts/Battle/Units/Enemies/MeleeEnemy.cs
+++ Desolate Wasteland/Assets/Scripts/Battle/Units/Enemies/MeleeEnemy.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MeleeEnemy : BaseEnemy
 {
-    // Start is called before the first frame update
-    void Start()
+    static readonly int maxHealth = 25;
+    static int currentHealth = 25;
+    int initiative;
+    static int quantity = 2;
+    public GameObject unitCounter;
+
+    public void Start()
     {
-        
+        setUnitCount();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void takeDamage(int dmg)
     {
-        
+        if (quantity > 0)
+        {
+            currentHealth -= dmg;
+            Debug.Log("Unit is taking " + dmg + " damage, currentHP: " + currentHealth);
+            if (currentHealth <= 0) //If unit health in stack <= 0
+            {
+                quantity--; //One unit in stack died
+                setUnitCount();
+
+                Debug.Log("Unit died, only " + quantity + " units left");
+
+                if (quantity <= 0)
+                {
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    currentHealth = maxHealth;
+                }
+            }
+        }
+
+    }
+
+    public void setUnitCount()
+    {
+        unitCounter.GetComponentInChildren<Text>().text = quantity.ToString();
     }
 }
