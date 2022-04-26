8046ded0416cc27f213b1c6e0399bb378cfbbebe
s21446 <s21446@pjwstk.edu.pl>
2022-04-27 00:02:06 +0200

    Main Menu and Pause Menu
    Wszystko powinno działać
    
    To-Do:
    Włączanie sceny pauzy w dowolnym momencie gry (poza walką)
    Powrót do Pozycji gracza na mapie

---
+++ Desolate Wasteland/Assets/Scripts/MainMenu/ButtonListButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListButton : MonoBehaviour
+{
+    public SaveManager manager;
+    public Text btnName;
+
+    public void setButtonName(string buttonName)
+    {
+        btnName.text = buttonName;
+    }
+
+    public void OnClick()
+    {
+        manager.load(btnName.text+"");
+    }
+}
---
+++ Desolate Wasteland/Assets/Scripts/MainMenu/MainMenuControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControls : MonoBehaviour
+{
+    public string _mainMapScene;
+    private string saveToLoad;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(_mainMapScene);
+        SaveSerial.NewGameSetData();
+
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(_mainMapScene);
+    }
+
+    public void LoadGameMenu()
+    {
+        //To call methods that load in save data
+        SceneManager.LoadScene(_mainMapScene);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
