9c6f5c0373a92f0f27c53fa884c25bddb9c35886
Grerek <grzegorz.brodzisz.2@gmail.com>
2022-05-18 10:51:32 +0200

    fix

--- Desolate Wasteland/Assets/Scripts/Battle/Units/Heroes/MeleeUnit.cs
+++ Desolate Wasteland/Assets/Scripts/Battle/Units/Heroes/MeleeUnit.cs
@@ -12,8 +12,7 @@ public class MeleeUnit : BaseHero
 
     static int feedness = 3; // 1 for each day of not being fed, after depleeted start decreasing health (-5 for each day?)
 
-    static int currentHealth = 25;
-    //public static int currentHealth = 25;    
+    public static int currentHealth = 25;  
 
     int movementSpeed;
     public static int initiative = 10;
